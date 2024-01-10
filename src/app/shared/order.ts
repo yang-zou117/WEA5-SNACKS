5daa7b27d3dd05e38c644f60652bed2a921c059c
yangzou2110307110 <S2110307110@students.fh-hagenberg.at>
2024-01-10 12:22:13 +0100

    some work

---
+++ src/app/shared/order.ts
@@ -0,0 +1,12 @@
+export class Order {
+    constructor(
+        orderId: number, 
+        restaurantId: number,
+        addressId:number, 
+        customerName: string, 
+        phoneNumber : string,
+        orderedDate: Date,
+        status: string,
+        orderCosts: number,
+    ){}
+}
